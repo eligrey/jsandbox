e140435cdd21a4ae17f3cfdbd8f0c00f372d94f7
Elijah Grey <elijah@grey.name>
2009-08-12 20:20:14 -0400

    Version 0.1.1 - now blocks access the MessageEvent sent to the worker

--- jsandbox.js
+++ jsandbox.js
@@ -1,21 +1,21 @@
 /*
-* jsandbox JavaScript Library v0.1
+* jsandbox JavaScript Library v0.1.1
 * 2009-06-22
 * By Elijah Grey, http://eligrey.com
 *
 * License: GNU GPL v3 and the X11/MIT license
-*   See http://eligrey.com/blog/about/license
+*   See README.md
 */
 
-if (typeof document == "undefined") // loaded as a worker
-	this.onmessage = function (event) { // recieved code
+if (typeof document === "undefined" && typeof JSON !== "undefined" && JSON.stringify) // loaded as a worker
+	this.onmessage = function (event) { // received code
 		var data = event.data;
 		
-		if (typeof data.code != "string")
-			throw new TypeError("code is not a string");
+		if (typeof data.code !== "string")
+			throw new TypeError("code must be a string");
 		
 		postMessage((function() {
-			with({ // block access to XMLHttpRequest, importScripts, and postMessage for the sandboxed code
+			with({ // block access to XMLHttpRequest, importScripts, and postMessage from the sandboxed code
 				window        : this, // provide a window object for compatibility with code that may use it
 				self          : this,
 				input         : this.input,
@@ -24,9 +24,10 @@ if (typeof document == "undefined") // loaded as a worker
 				postMessage   : undefined,
 				XMLHttpRequest: undefined,
 									
-				arguments     : undefined // dereference this function
+				event         : undefined, // dereference the MessageEvent and this function
+				arguments     : undefined
 			})
-				return eval("with({data:undefined}){eval("+JSON.stringify(data.code)+")}"); // dereference data object and eval data.code
+				return eval("with({data:undefined})eval(" + JSON.stringify(data.code) + ")"); // dereference data object and eval data.code
 				// the above line must be hand-minified if you minified the name of the data object
 		}).call({input: data.input}));
 	}; // you should terminate this worker now as it may have been tainted with new globals
@@ -45,12 +46,7 @@ else (function() { // loaded in a document
 		jsandbox.uri = scriptNode.getAttribute("src");
 	
 	else // create an error to get the file URI
-		try {
-			throw new Error();
-		} catch (e) {
-			if ("fileName" in e)
-				jsandbox.uri = e.fileName;
-		}
+		jsandbox.uri = (new Error).fileName;
 	
 	
 	jsandbox.eval = function(options) {
@@ -61,7 +57,7 @@ else (function() { // loaded in a document
 	[OPTIONAL]  onerror  : Callback to pass an exception if one is thrown opon eval()ing the code.
 	[OPTIONAL]  uri      : URI of worker script. Defaults to jsandbox.uri.
 	*/
-		var worker = new Worker(options.uri||jsandbox.uri),
+		var worker = new Worker(options.uri || jsandbox.uri),
 		request    = { // request object
 			code: options.code
 		};
@@ -72,20 +68,20 @@ else (function() { // loaded in a document
 		worker.onmessage = function(event) {
 			worker.terminate();
 			
-			if (typeof options.callback == "function")
+			if (typeof options.callback === "function")
 				options.callback(event.data);
 		};
 		worker.onerror = function(e) {
 			worker.terminate();
 			
-			if (typeof options.onerror == "function")
+			if (typeof options.onerror === "function")
 				options.onerror(e);
 		};
 
-		worker.postMessage(request); // send the request object
+		worker.postMessage(request);
 		
 		return jsandbox;
 	};
 
 	this.jsandbox = jsandbox;
-}).call(this);
+}.call(this));
